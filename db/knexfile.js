c019d6d1cdcff4fe0df5d709831b4d3858f477c2
Deeptha <deeptha1618@gmail.com>
2022-02-18 11:29:46 +0530

    Knex setup and create migrations

---
+++ db/knexfile.js
@@ -0,0 +1,38 @@
+module.exports = {
+
+    development: {
+        client: 'mysql',
+        connection: {
+            host: '127.0.0.1',
+            user: 'sqluser',
+            password: 'password',
+            database: 'golabi',
+            charset: 'utf8',
+        },
+        migrations: {
+            tableName: 'knex_migrations' 
+        },
+        seeds: {
+            directory: './seeds' 
+        }
+    }
+
+};
+
+// const knex = require('knex')({
+//   client: 'mysql',
+//   connection: {
+//     host : 'localhost',
+//     port : 3306,
+//     user : 'sqluser',
+//     password : '',
+//     database : 'knex-tutorial'
+//   },
+//   pool: { min: 0, max: 7 }
+// });
+//  //check that connection works
+//  knex.raw("SELECT VERSION()").then(()=>{
+//    console.log('Connection to db successful');
+//  });
+
+// module.exports =knex;
